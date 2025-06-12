358c2aa926b5523ee172682f016ad4d93d1db278
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-06-12 15:29:37 +0500

    commit

--- js/register.js
+++ js/register.js
@@ -1,13 +1,13 @@
 'use strict';
 
 (async _ => {
-  const statistics = new Statistics();
+  // const statistics = new Statistics();
   let user = JSON.parse(localStorage.getItem('user'));
 
   const joinButton = document.querySelector('#joinToChannel')
   joinButton?.addEventListener('click', async (e) => {
     joinButton.disabled = true;
-    await statistics.onClickTgBtn();
+    // await statistics.onClickTgBtn();
     joinButton.disabled = false;
     window.location.href = 'https://t.me/+K2HxXVNGDnpiOGQy';
   })
@@ -18,15 +18,15 @@
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    formData.append(`Foydalanuvchi ID`, statistics.userId);
-    formData.append(`Timestamp`, statistics.time?.toString());
+    // formData.append(`Foydalanuvchi ID`, statistics.userId);
+    // formData.append(`Timestamp`, statistics.time?.toString());
 
     let response = await fetch('https://script.google.com/macros/s/AKfycby_w9FPXJxpMagLyIH8tFTQf1dgh4_-UgH0ACS8q00upcgp43sK8q4FTJ7L4Kn8kV18CA/exec', {
       method: 'POST',
       body: formData
     })
     await response.json();
-    await statistics.onRegister(user);
+    // await statistics.onRegister(user);
     localStorage.removeItem('user');
   } else {
     localStorage.removeItem('user');
